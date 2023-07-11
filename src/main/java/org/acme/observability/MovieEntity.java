6b703bcaf68eeeb66d939ff12badf2d5985fc5ae
patterncatalyst <rsedor@patterncatalyst.io>
2023-07-10 22:47:40 -0400

    Updated MovieEntity with namedquery

--- src/main/java/org/acme/observability/MovieEntity.java
+++ src/main/java/org/acme/observability/MovieEntity.java
@@ -1,20 +1,17 @@
 package org.acme.observability;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 //import io.quarkus.hibernate.orm.panache.PanacheEntity;
 //import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 
 @Entity
 @Table(name = "movie")
+@NamedQuery(name = "Movies.findAll", query = "SELECT m FROM MovieEntity m ORDER BY m.title")
 public class MovieEntity {
 
     @Id
     @SequenceGenerator(name = "moviesSequence", sequenceName = "known_movies_id_seq", allocationSize = 1, initialValue = 10)
     @GeneratedValue(generator = "moviesSequence")
-    private Integer id;
+    public Integer id;
 
     public String title;
     public Integer year;
--- src/main/java/org/acme/observability/MovieResource.java
+++ src/main/java/org/acme/observability/MovieResource.java
@@ -1,13 +1,23 @@
 package org.acme.observability;
+import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.hibernate.reactive.mutiny.Mutiny;
+import org.jboss.logging.Logger;
+
+import java.util.List;
 
 @Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class MovieResource {
+
+    private static final Logger LOGGER = Logger.getLogger(MovieResource.class);
+
+    @Inject
+    Mutiny.SessionFactory sf;
 
     @Inject
     MovieProducer producer;
@@ -19,9 +29,12 @@ public class MovieResource {
         return Response.accepted().build();
     }
 
-//    @GET
-//    @Path("movies")
-//    public List<MovieEntity> getMovies() {
-//        return MovieEntity.listAll(Sort.by("year"));
-//    }
+    @GET
+    @Path("/movies")
+    public Uni<List<MovieEntity>> get() {
+        return sf.withTransaction((s,t) -> s
+                .createNamedQuery("Movies.findAll", MovieEntity.class)
+                .getResultList()
+        );
+    }
 }
