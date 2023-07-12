de68ed6e4cc55506cee305384066a7b95731efc6
patterncatalyst <rsedor@patterncatalyst.io>
2023-07-12 17:48:52 -0400

    removed repository and dto movie classes

--- src/main/java/org/acme/observability/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package org.acme.observability;
-
-import io.quarkus.hibernate.reactive.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class MovieRepository implements PanacheRepository<MovieEntity> {
-}
