f06452a684e76c12be3ab7860570e6b4487feb9f
patterncatalyst <rsedor@patterncatalyst.io>
2023-07-12 17:47:46 -0400

    updated crud operations

--- src/main/java/org/acme/observability/MovieResource.java
+++ src/main/java/org/acme/observability/MovieResource.java
@@ -1,6 +1,13 @@
 package org.acme.observability;
+
+import static jakarta.ws.rs.core.Response.Status.CREATED;
+import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
+import static jakarta.ws.rs.core.Response.Status.NO_CONTENT;
+
+import io.micrometer.core.annotation.Counted;
 import io.micrometer.core.annotation.Timed;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
+
 import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
@@ -25,12 +32,20 @@ public class MovieResource {
     MovieProducer producer;
 
     @POST
+    @Path("/movies")
     @WithSpan("MovieResource.send")
     @Timed(value = "MovieResource.send")
+    @Counted
     public Response send(MovieEntity movieEntity) {
         producer.sendMovieToKafka(movieEntity);
         // Return an 202 - Accepted response.
         return Response.accepted().build();
+    }
+
+    @GET
+    @Path("/movies/{id}")
+    public Uni<MovieEntity> getById(Integer id) {
+        return sf.withTransaction((s,t) -> s.find(MovieEntity.class, id));
     }
 
     @GET
@@ -40,5 +55,32 @@ public class MovieResource {
                 .createNamedQuery("Movies.findAll", MovieEntity.class)
                 .getResultList()
         );
+    }
+
+    @PUT
+    @Path("/movies/{id}")
+    public Uni<Response> update(Integer id, MovieEntity movieEntity) {
+        if (movieEntity == null || movieEntity.title == null) {
+            throw new WebApplicationException("Movie title was not set on request.", 422);
+        }
+
+        return sf.withTransaction((s,t) -> s.find(MovieEntity.class, id)
+                        .onItem().ifNull().failWith(new WebApplicationException("Movie missing from database.", NOT_FOUND))
+                        // If entity exists then update it
+                        .invoke(entity -> {
+                            entity.title = movieEntity.title;
+                            entity.year = movieEntity.year;
+                        }))
+                .map(entity -> Response.ok(entity).build());
+    }
+
+    @DELETE
+    @Path("/movies/{id}")
+    public Uni<Response> delete(Integer id) {
+        return sf.withTransaction((s,t) -> s.find(MovieEntity.class, id)
+                        .onItem().ifNull().failWith(new WebApplicationException("Movie missing from database.", NOT_FOUND))
+                        // If entity exists then delete it
+                        .call(s::remove))
+                .replaceWith(Response.ok().status(NO_CONTENT)::build);
     }
 }
